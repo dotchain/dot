5af340d5575e39297ae02e02f0daf80b5a8ee798
Ramesh VK <ramesh.vyaghrapuri@gmail.com>
2018-01-09 17:52:58 -0800

    Move guessEmptyValue out of the test

--- reconstruct_test.go
+++ reconstruct_test.go
@@ -10,50 +10,6 @@ import (
 )
 
 func TestUtilsReconstruct(t *testing.T) {
-	guessEmptyValue := func(ops []dot.Change) (interface{}, bool) {
-		if len(ops) == 0 {
-			return nil, true
-		}
-		op := ops[0]
-		u := dot.Utils(dot.Transformer{})
-
-		if len(op.Path) > 0 {
-			panic("Unexpected path with empty before")
-		}
-
-		if op.Splice != nil {
-			switch {
-			case op.Splice.Before != nil:
-				if val, ok := u.C.TryGet(op.Splice.Before); ok {
-					return val.Slice(0, 0), true
-				}
-				panic("Unexpected splice op")
-			case op.Splice.After != nil:
-				if val, ok := u.C.TryGet(op.Splice.After); ok {
-					return val.Slice(0, 0), true
-				}
-				panic("Unexpected splice op")
-			}
-			return nil, false
-		}
-		if op.Set != nil {
-			return map[string]interface{}{}, true
-		}
-		if op.Move != nil {
-			if op.Move.Count == 0 || op.Move.Distance == 0 {
-				return nil, false
-			}
-			panic("Unexpected move on nil input")
-		}
-		if op.Range != nil {
-			if op.Range.Count == 0 {
-				return nil, false
-			}
-			panic("Unexpected range on nil input")
-		}
-		return nil, false
-	}
-
 	validate := func(m interface{}) {
 		u := dot.Utils(dot.Transformer{})
 		changes := u.Reconstruct(m)
@@ -77,4 +33,48 @@ func TestUtilsReconstruct(t *testing.T) {
 	validate(map[string]interface{}{})
 	validate(map[string]interface{}{"p": 42, "q": "hello"})
 	validate(map[string]interface{}{"p": []interface{}{"a", 42.0}})
+}
+
+func guessEmptyValue(ops []dot.Change) (interface{}, bool) {
+	if len(ops) == 0 {
+		return nil, true
+	}
+	op := ops[0]
+	u := dot.Utils(dot.Transformer{})
+	
+	if len(op.Path) > 0 {
+		panic("Unexpected path with empty before")
+	}
+	
+	if op.Splice != nil {
+		switch {
+		case op.Splice.Before != nil:
+			if val, ok := u.C.TryGet(op.Splice.Before); ok {
+				return val.Slice(0, 0), true
+			}
+			panic("Unexpected splice op")
+		case op.Splice.After != nil:
+			if val, ok := u.C.TryGet(op.Splice.After); ok {
+				return val.Slice(0, 0), true
+			}
+			panic("Unexpected splice op")
+		}
+		return nil, false
+	}
+	if op.Set != nil {
+		return map[string]interface{}{}, true
+	}
+	if op.Move != nil {
+		if op.Move.Count == 0 || op.Move.Distance == 0 {
+			return nil, false
+		}
+		panic("Unexpected move on nil input")
+	}
+	if op.Range != nil {
+		if op.Range.Count == 0 {
+			return nil, false
+		}
+		panic("Unexpected range on nil input")
+	}
+	return nil, false
 }
