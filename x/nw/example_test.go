f537aa9f736fa811bdfc01dfcd4f46117af3c9bd
Ramesh VK <ramesh.vyaghrapuri@gmail.com>
2018-09-16 15:00:56 -0700

    Add simple nw/ access to Ops

---
+++ x/nw/example_test.go
@@ -0,0 +1,46 @@
+// Copyright (C) 2018 Ramesh Vyaghrapuri. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+package nw_test
+
+import (
+	"fmt"
+	"github.com/dotchain/dot/changes"
+	"github.com/dotchain/dot/ops"
+	"github.com/dotchain/dot/x/nw"
+	"net/http/httptest"
+)
+
+func Example() {
+	handler := &nw.Handler{Store: MemStore(nil)}
+	srv := httptest.NewServer(handler)
+	defer srv.Close()
+
+	c := nw.Client{URL: srv.URL, Client: srv.Client()}
+	defer c.Close()
+
+	op1 := ops.Operation{"ID1", "", 100, -1, nil}
+	op2 := ops.Operation{"ID2", "ID1", 100, -1, changes.ChangeSet{changes.Move{1, 2, 3}}}
+
+	ctx := getContext()
+	if err := c.Append(ctx, []ops.Op{op1}); err != nil {
+		fmt.Println("Append1", err)
+		return
+	}
+	if err := c.Append(ctx, []ops.Op{op2}); err != nil {
+		fmt.Println("Append2", err)
+		return
+	}
+
+	if err := c.Poll(ctx, 1); err != nil {
+		fmt.Println("Poll", err)
+		return
+	}
+
+	ops, err := c.GetSince(ctx, 0, 100)
+	fmt.Println("Ops", ops, err)
+
+	// Output:
+	// Ops [{ID1  0 -1 <nil>} {ID2 ID1 1 -1 [{1 2 3}]}] <nil>
+}
---
+++ x/nw/nw_test.go
@@ -0,0 +1,140 @@
+// Copyright (C) 2018 Ramesh Vyaghrapuri. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+package nw_test
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"github.com/dotchain/dot/changes"
+	"github.com/dotchain/dot/ops"
+	"github.com/dotchain/dot/x/nw"
+	"github.com/dotchain/dot/x/rt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestClientErrors(t *testing.T) {
+	c := &nw.Client{URL: "http://localhost:8183/nw?q=1"}
+
+	// unknown type
+	op1 := ops.Operation{"ID1", "", 100, -1, rt.Run{}}
+	err := c.Append(getContext(), []ops.Op{op1})
+	if err == nil {
+		t.Fatal("Did not fail with unregistered change type")
+	}
+
+	c = &nw.Client{URL: "&amp;://ok"}
+	err = c.Append(getContext(), []ops.Op{})
+	if err == nil {
+		t.Fatal("Did not fail with invalid url")
+	}
+
+	// simulate server error
+	errout := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "boo", 300)
+	})
+	srv := httptest.NewServer(errout)
+	defer srv.Close()
+
+	c = &nw.Client{URL: srv.URL, Client: srv.Client()}
+
+	err = c.Append(getContext(), []ops.Op{})
+	if err == nil {
+		t.Fatal("Unexpected success with client error")
+	}
+
+	srv2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("gooopey"))
+	}))
+	defer srv2.Close()
+
+	c = &nw.Client{URL: srv2.URL, Client: srv2.Client()}
+	err = c.Append(getContext(), []ops.Op{})
+	if err == nil {
+		t.Fatal("Unexpected success with client error")
+	}
+}
+
+func TestHeaderPasssing(t *testing.T) {
+	foundHeader := false
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		foundHeader = r.Header.Get("Zug") == "Zug"
+		w.Write(nil)
+	})
+	srv := httptest.NewServer(handler)
+	defer srv.Close()
+
+	h := http.Header{}
+	h.Set("Zug", "Zug")
+	c := &nw.Client{URL: srv.URL, Client: srv.Client(), Header: h}
+	c.Append(getContext(), []ops.Op{})
+	if !foundHeader {
+		t.Fatal("Missed Zug Header")
+	}
+}
+
+func TestServerErrors(t *testing.T) {
+	store := fakeStore{}
+	srv := httptest.NewServer(&nw.Handler{Store: store})
+	defer srv.Close()
+
+	resp, err := srv.Client().Get(srv.URL)
+	if err != nil || resp.StatusCode != 400 {
+		t.Fatal("Unexpected fetch without content-type", err, resp)
+	}
+
+	data := bytes.NewReader([]byte("Some garbage"))
+	resp, err = srv.Client().Post(srv.URL, "application/x-gob", data)
+	if err != nil || resp.StatusCode != 400 {
+		t.Fatal("Unexpected fetch without content-type", err, resp)
+	}
+
+	c := &nw.Client{URL: srv.URL, Client: srv.Client()}
+	err = c.Append(getContext(), nil)
+	if err == nil || err.Error() != "Append error" {
+		t.Fatal("Unexpected append behavior", err)
+	}
+
+	opx, err := c.GetSince(getContext(), 0, 0)
+	if err == nil {
+		t.Fatal("Unexpected success for GetSince", err, opx)
+	}
+}
+
+type fakeStore struct{}
+
+func (f fakeStore) Append(_ context.Context, opx []ops.Op) error {
+	return errors.New("Append error")
+}
+
+func (f fakeStore) GetSince(_ context.Context, version, limit int) ([]ops.Op, error) {
+	unencodeable := []ops.Op{unencodeableOp{}}
+	return unencodeable, errors.New("GetSince error")
+}
+
+func (f fakeStore) Poll(_ context.Context, version int) error {
+	return errors.New("Poll error")
+}
+
+func (f fakeStore) Close() {
+}
+
+type unencodeableOp struct{}
+
+func (u unencodeableOp) ID() interface{}                   { return nil }
+func (u unencodeableOp) Version() int                      { return 0 }
+func (u unencodeableOp) WithVersion(int) ops.Op            { return nil }
+func (u unencodeableOp) Parent() interface{}               { return nil }
+func (u unencodeableOp) Basis() int                        { return 0 }
+func (u unencodeableOp) Changes() changes.Change           { return nil }
+func (u unencodeableOp) WithChanges(changes.Change) ops.Op { return nil }
+
+func getContext() context.Context {
+	ctx, _ := context.WithDeadline(context.Background(), time.Now().Add(3*time.Second))
+	return ctx
+}
