59c6cb0ff8519bc80bac6a8c68b653f534fa33a6
Ramesh VK <ramesh.vyaghrapuri@gmail.com>
2018-10-08 00:06:15 +0530

    Update documentation

--- streams/stream.go
+++ streams/stream.go
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file.
 
+// Package streams defines the types for managing a sequence of
+// changes
 package streams
 
 import "github.com/dotchain/dot/changes"
