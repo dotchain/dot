234657f7ce73ce3586af42f72ae756775eebec6f
Ramesh VK <ramesh.vyaghrapuri@gmail.com>
2018-10-13 08:29:05 -0500

    Remove older dot/ package in favor of dot/changes, dot/streams etc

---
+++ dot.go
@@ -0,0 +1,15 @@
+// Copyright (C) 2018 Ramesh Vyaghrapuri. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+// Package dot is a placeholder.
+//
+// The core functionality is spread out between dot/changes,
+// dot/refs, dot.streams and dot/x.
+//
+// At some point this package will provide a simple interface
+// to the whole project.
+//
+// Please see [github](https://gitbub.com/dotchain/dot) for
+// details about the project
+package dot
