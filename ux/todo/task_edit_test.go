94a275e3d552e80aee888cf37ff7ecfba575e6a8
Ramesh VK <ramesh@dotchain.org>
2019-02-21 18:01:10 -0800

    Refactor with code-generation

---
+++ ux/todo/task_edit_test.go
@@ -0,0 +1,29 @@
+// Copyright (C) 2019 rameshvk. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+package todo_test
+
+import (
+	"fmt"
+	"github.com/dotchain/dot/ux"
+	"github.com/dotchain/dot/ux/todo"
+)
+
+func Example_renderTask() {
+	task := todo.Task{Done: false, Description: "first task"}
+	t := todo.NewTaskEdit(ux.Styles{Color: "blue"}, task)
+	fmt.Println(t.Root)
+
+	t.Update(ux.Styles{Color: "red"}, task)
+	fmt.Println(t.Root)
+
+	task.Done = true
+	t.Update(ux.Styles{Color: "red"}, task)
+	fmt.Println(t.Root)
+
+	// Output:
+	// div[styles:{blue}]( input[type:checkbox]() input[type:text](first task))
+	// div[styles:{red}]( input[type:checkbox]() input[type:text](first task))
+	// div[styles:{red}]( input[type:checkbox checked]() input[type:text](first task))
+}
