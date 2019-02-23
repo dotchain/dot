ffdbb91f691f95201714bbac949faf7bc6c6ffff
Ramesh VK <ramesh@dotchain.org>
2019-02-23 14:37:18 -0800

    ux: documentation update

--- ux/todo/app.go
+++ ux/todo/app.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a MIT-style license
 // that can be found in the LICENSE file.
 
+// Package todo is an example TODO task list app
 package todo
 
 import (
