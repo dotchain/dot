d05b2957983c9a0ad66ed6eadfd05d316ff97b58
Ramesh VK <ramesh@dotchain.org>
2019-03-23 23:46:26 -0700

    x/dotc: simplify by removing Nullable

--- x/dotc/testdata/mystruct/generated.go
+++ x/dotc/testdata/mystruct/generated.go
@@ -11,24 +11,11 @@ func (my myStruct) get(key interface{}) changes.Value {
 	case "b":
 		return changes.Atomic{my.boo}
 	case "bp":
-		if my.boop == nil {
-			return changes.Nil
-		}
 		return changes.Atomic{my.boop}
 	case "s":
 		return types.S16(my.str)
 	case "s16":
 		return my.str16
-	case "x":
-		if my.xtr == nil {
-			return changes.Nil
-		}
-		return types.S16(*my.xtr)
-	case "x16":
-		if my.xtr16 == nil {
-			return changes.Nil
-		}
-		return *my.xtr16
 	}
 	panic(key)
 }
@@ -39,27 +26,11 @@ func (my myStruct) set(key interface{}, v changes.Value) changes.Value {
 	case "b":
 		myClone.boo = v.(changes.Atomic).Value.(bool)
 	case "bp":
-		if v == changes.Nil {
-			myClone.boop = nil
-			break
-		}
 		myClone.boop = v.(changes.Atomic).Value.(*bool)
 	case "s":
 		myClone.str = string(v.(types.S16))
 	case "s16":
 		myClone.str16 = v.(types.S16)
-	case "x":
-		if v == changes.Nil {
-			myClone.xtr = nil
-			break
-		}
-		myClone.xtr = func(x string) *string { return &x }(string(v.(types.S16)))
-	case "x16":
-		if v == changes.Nil {
-			myClone.xtr16 = nil
-			break
-		}
-		myClone.xtr16 = func(x types.S16) *types.S16 { return &x }(v.(types.S16))
 	default:
 		panic(key)
 	}
