970031401b6ae9dcc853c0d5adec4e045ec371c5
Ramesh VK <ramesh@dotchain.org>
2019-03-27 13:04:56 -0700

    dot/streams: add Transform()

---
+++ streams/transform.go
@@ -0,0 +1,54 @@
+// Copyright (C) 2018 rameshvk. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+package streams
+
+import "github.com/dotchain/dot/changes"
+
+// Transform creates a new stream where changes are modified before
+// being appended or before being fetched.
+//
+// Both next and append are optional.
+//
+// Note that nil is allowed both as argument and return for next and
+// append.
+func Transform(parent Stream, append, next func(changes.Change) changes.Change) Stream {
+	if append == nil {
+		append = func(c changes.Change) changes.Change { return c }
+	}
+	if next == nil {
+		next = func(c changes.Change) changes.Change { return c }
+	}
+	return transform{parent, append, next}
+}
+
+type transform struct {
+	parent       Stream
+	append, next func(changes.Change) changes.Change
+}
+
+func (t transform) Next() (Stream, changes.Change) {
+	next, nextc := t.parent.Next()
+	if next == nil {
+		return nil, nil
+	}
+
+	nextc = t.next(nextc)
+	return transform{next, t.append, t.next}, nextc
+}
+
+func (t transform) Nextf(key interface{}, fn func()) {
+	t.parent.Nextf(key, fn)
+}
+
+func (t transform) Append(c changes.Change) Stream {
+	if c = t.append(c); c != nil {
+		return transform{t.parent.Append(c), t.append, t.next}
+	}
+	return t
+}
+
+func (t transform) ReverseAppend(c changes.Change) Stream {
+	return transform{t.parent.ReverseAppend(c), t.append, t.next}
+}
