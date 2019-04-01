e40c454adf8ad16d20561ae02f70aa069524c358
Ramesh VK <ramesh@dotchain.org>
2019-04-01 16:12:16 -0700

    fix data race issues in test

--- x/coverage.sh
+++ x/coverage.sh
@@ -5,5 +5,5 @@ set -e
 for d in $(go list ./... | grep -v vendor | grep -v /cmd/ | grep -v /tools/ | grep -v /demo/ | grep -v /testing/); do
     out=$(echo $d | cut -c21- | sed "s/\//_/g")
     rm -f coverage$out
-    go test -coverprofile=coverage$out -covermode=atomic -tags integration $d
+    go test -race -coverprofile=coverage$out -covermode=atomic -tags integration $d
 done
