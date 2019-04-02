36e7952be30d5e20bc3530f1d4a6ef7a2e00fae3
Ramesh VK <ramesh@dotchain.org>
2019-04-02 14:16:23 -0700

    dot/stress: add initial stress app
    The stress app can be run by doing `go test --tags=stress`.
    
    The stress app seems to succeed and only runs a few seconds and does not include all possible operations and types.
    
    But it is a decent start.

---
+++ stress/codegen.go
@@ -0,0 +1,50 @@
+// Copyright (C) 2019 rameshvk. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+//+build ignore
+
+package main
+
+import (
+	"io/ioutil"
+
+	"github.com/dotchain/dot/x/dotc"
+)
+
+func main() {
+	code, err := info.Generate()
+	if err != nil {
+		panic(err)
+	}
+	code = "//+build stress\n\n" + code
+	err = ioutil.WriteFile("generated_test.go", []byte(code), 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+var info = dotc.Info{
+	Package: "stress_test",
+	Structs: []dotc.Struct{{
+		Recv: "s",
+		Type: "State",
+		Fields: []dotc.Field{{
+			Name:               "Text",
+			Key:                "text",
+			Type:               "string",
+			ToValueFmt:         "types.S8(%s)%.s",
+			FromValueFmt:       "string(%s.(types.S8))%.s",
+			FromStreamValueFmt: "%s%.s",
+			ToStreamFmt:        "streams.S8%.s",
+		}, {
+			Name:               "Count",
+			Key:                "count",
+			Type:               "types.Counter",
+			ToValueFmt:         "%s%.s",
+			FromValueFmt:       "%s.(types.Counter)%.s",
+			FromStreamValueFmt: "int32(%s)%.s",
+			ToStreamFmt:        "streams.Counter%.s",
+		}},
+	}},
+}
