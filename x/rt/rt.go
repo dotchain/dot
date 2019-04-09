266d96926d396a7a0e2f032da92109badd0bea8a
Ramesh VK <ramesh@dotchain.org>
2019-04-08 17:31:28 -0700

    Remove unstable parts of the code

--- x/rt/rt.go
+++
@@ -1,7 +0,0 @@
-// Copyright (C) 2018 rameshvk. All rights reserved.
-// Use of this source code is governed by a MIT-style license
-// that can be found in the LICENSE file.
-
-// Package rt implements a rich type value which supports formatting,
-// styles, lists etc
-package rt
