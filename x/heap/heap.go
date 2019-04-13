2c743672108a3ebc00816a9a4af117ec21c3a444
Ramesh VK <ramesh@dotchain.org>
2019-04-13 09:35:04 -0700

    x/heap: make Heap serializable with gob/encoding

--- x/heap/heap.go
+++ x/heap/heap.go
@@ -14,31 +14,31 @@ import (
 	"github.com/dotchain/dot/changes/types"
 )
 
-type entry struct {
-	key1, key2 int64
-	rank       int
+type Entry struct {
+	Key1, Key2 int64
+	Rank       int
 }
 
 // Heap implements a heap of keys
 type Heap struct {
-	items map[interface{}]entry
+	Items map[interface{}]Entry
 }
 
 // UpdateChange returns the change to apply for either inserting an
-// item into the heap or updating its rank.
+// item into the heap or updating its Rank.
 func (h Heap) UpdateChange(key interface{}, rank int) changes.Change {
-	if entry, ok := h.items[key]; ok && entry.rank == rank {
+	if Entry, ok := h.Items[key]; ok && Entry.Rank == rank {
 		return nil
 	}
 
-	v := changes.Atomic{Value: entry{key1: rand.Int63(), key2: rand.Int63(), rank: rank}}
+	v := changes.Atomic{Value: Entry{Key1: rand.Int63(), Key2: rand.Int63(), Rank: rank}}
 	p := []interface{}{key}
 	return changes.PathChange{Path: p, Change: changes.Replace{Before: h.get(key), After: v}}
 }
 
 // DeleteChange returns the change to apply for deleting an item from the heap
 func (h Heap) DeleteChange(key interface{}) changes.Change {
-	if _, ok := h.items[key]; !ok {
+	if _, ok := h.Items[key]; !ok {
 		return nil
 	}
 
@@ -46,13 +46,13 @@ func (h Heap) DeleteChange(key interface{}) changes.Change {
 	return changes.PathChange{Path: p, Change: changes.Replace{Before: h.get(key), After: changes.Nil}}
 }
 
-// Update inserts a new entry wite provided rank or updates the rank
-// for an existng entry
+// Update inserts a new Entry wite provided Rank or updates the Rank
+// for an existng Entry
 func (h Heap) Update(key interface{}, rank int) Heap {
 	return h.Apply(nil, h.UpdateChange(key, rank)).(Heap)
 }
 
-// Delete removes the entry from the heap
+// Delete removes the Entry from the heap
 func (h Heap) Delete(key interface{}) Heap {
 	return h.Apply(nil, h.DeleteChange(key)).(Heap)
 }
@@ -62,47 +62,47 @@ func (h Heap) Apply(ctx changes.Context, c changes.Change) changes.Value {
 	return (types.Generic{Get: h.get, Set: h.set}).Apply(ctx, c, h)
 }
 
-// Iterate calls the provided function for each entry in the heap in
-// descending order of rank value.
+// Iterate calls the provided function for each Entry in the heap in
+// descending order of Rank value.
 func (h Heap) Iterate(fn func(key interface{}, rank int) bool) {
-	keys := make([]interface{}, 0, len(h.items))
-	for k := range h.items {
+	keys := make([]interface{}, 0, len(h.Items))
+	for k := range h.Items {
 		keys = append(keys, k)
 	}
 	sort.Slice(keys, func(i, j int) bool {
-		v1 := h.items[keys[j]]
-		v2 := h.items[keys[i]]
-		if v1.rank != v2.rank {
-			return v1.rank < v2.rank
+		v1 := h.Items[keys[j]]
+		v2 := h.Items[keys[i]]
+		if v1.Rank != v2.Rank {
+			return v1.Rank < v2.Rank
 		}
-		if v1.key1 != v2.key1 {
-			return v1.key1 < v2.key1
+		if v1.Key1 != v2.Key1 {
+			return v1.Key1 < v2.Key1
 		}
-		return v1.key2 < v2.key2
+		return v1.Key2 < v2.Key2
 	})
 	for _, key := range keys {
-		if !fn(key, h.items[key].rank) {
+		if !fn(key, h.Items[key].Rank) {
 			break
 		}
 	}
 }
 
 func (h Heap) get(key interface{}) changes.Value {
-	if entry, ok := h.items[key]; ok {
-		return changes.Atomic{Value: entry}
+	if Entry, ok := h.Items[key]; ok {
+		return changes.Atomic{Value: Entry}
 	}
 	return changes.Nil
 }
 
 func (h Heap) set(key interface{}, v changes.Value) changes.Value {
-	clone := Heap{items: map[interface{}]entry{}}
-	for k, v := range h.items {
+	clone := Heap{Items: map[interface{}]Entry{}}
+	for k, v := range h.Items {
 		if k != key {
-			clone.items[k] = v
+			clone.Items[k] = v
 		}
 	}
 	if v != changes.Nil {
-		clone.items[key] = v.(changes.Atomic).Value.(entry)
+		clone.Items[key] = v.(changes.Atomic).Value.(Entry)
 	}
 	return clone
 }
