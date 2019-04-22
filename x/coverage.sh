62df0166324a46b3cf28b74b9649bf10deec78c5
Ramesh VK <ramesh@dotchain.org>
2019-04-21 18:26:30 -0700

    Add go.mod at root

--- x/coverage.sh
+++ x/coverage.sh
@@ -4,12 +4,12 @@ set -e
 
 if [ "$TRAVIS_EVENT_TYPE" == "cron" ]
 then
-    go test -v ./stress -race -tags stress -rounds 20 -clients 5 -iterations 5
+    GO111MODULE=on go test -v ./stress -race -tags stress -rounds 20 -clients 5 -iterations 5
     exit $?
 fi
 
 for d in $(go list ./... | grep -v vendor | grep -v /cmd/ | grep -v /tools/ | grep -v /demo/ | grep -v /testing/); do
     out=$(echo $d | cut -c21- | sed "s/\//_/g")
     rm -f coverage$out
-    go test -race -coverprofile=coverage$out -covermode=atomic -tags "integration stress" $d
+    GO111MODULE=on go test -race -coverprofile=coverage$out -covermode=atomic -tags "integration stress" $d
 done
