8bf83a304fc1d31b8396c53accef2d374adcce3b
Ramesh VK <ramesh@dotchain.org>
2019-05-15 12:24:36 -0700

    Disable -race since pq seems to fail quite often

--- x/coverage.sh
+++ x/coverage.sh
@@ -11,5 +11,6 @@ fi
 for d in $(go list ./... | grep -v vendor | grep -v /cmd/ | grep -v /tools/ | grep -v /demo/ | grep -v /testing/); do
     out=$(echo $d | cut -c21- | sed "s/\//_/g")
     rm -f coverage$out
-    GO111MODULE=on go test -race -coverprofile=coverage$out -covermode=atomic -tags "integration stress" $d
+    # Add -race back to this once pq data races get fixed
+    GO111MODULE=on go test -coverprofile=coverage$out -covermode=atomic -tags "integration stress" $d
 done
