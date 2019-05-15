e9b398f8f525e978a363ac14d0bb88066b9a9828
Ramesh VK <ramesh@dotchain.org>
2019-05-15 13:02:03 -0700

    fred: Switch Scope to use DefMap

--- fred/scope.go
+++ fred/scope.go
@@ -4,62 +4,37 @@
 
 package fred
 
-import (
-	"github.com/dotchain/dot/changes"
-	"github.com/dotchain/dot/changes/types"
-)
-
-// Scope implements a simple resolver
-type Scope map[interface{}]Def
-
-func (s Scope) set(key interface{}, value changes.Value) changes.Value {
-	clone := Scope{}
-	for k, v := range s {
-		if k != key {
-			clone[k] = v
-		}
-	}
-
-	if value != changes.Nil {
-		clone[key] = value.(Def)
-	}
-	return clone
-}
-
-func (s Scope) get(key interface{}) changes.Value {
-	if v, ok := s[key]; ok {
-		return v
-	}
-	return changes.Nil
-}
-
-// Apply implements the changes.Value interface
-func (s Scope) Apply(ctx changes.Context, c changes.Change) changes.Value {
-	return (types.Generic{Set: s.set, Get: s.get}).Apply(ctx, c, s)
+// Scope converts a DefMap into a scope/resolver
+type Scope struct {
+	// DefMap is not anonymous to ensure Scope is not treated as a Val or Def itself
+	DefMap *DefMap
 }
 
 // Resolve implements the Resolver interface
 func (s Scope) Resolve(key interface{}) (Def, Resolver) {
-	if def, ok := s[key]; ok {
+	if s.DefMap == nil {
+		return nil, nil
+	}
+
+	if def, ok := (*s.DefMap)[key]; ok {
 		return def, s
 	}
 	return nil, nil
 }
 
-// WithParent returns a chained resolver
-func (s Scope) WithParent(r Resolver) Resolver {
-	return withParentScope{s, r}
+// ChainResolver returns a resolver that tries the child and then parent.
+func ChainResolver(child, parent Resolver) Resolver {
+	return chainResolver{child, parent}
 }
 
-type withParentScope struct {
-	self   Scope
-	parent Resolver
+type chainResolver struct {
+	child, parent Resolver
 }
 
-func (s withParentScope) Resolve(key interface{}) (Def, Resolver) {
-	def, r := s.self.Resolve(key)
+func (c chainResolver) Resolve(key interface{}) (Def, Resolver) {
+	def, r := c.child.Resolve(key)
 	if r == nil {
-		def, r = s.parent.Resolve(key)
+		def, r = c.parent.Resolve(key)
 	}
 	return def, r
 }
