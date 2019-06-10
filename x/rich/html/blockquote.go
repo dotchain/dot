b1d7ec5645e0cad2888aa1bc8c94203e299dc687
Ramesh VK <ramesh@dotchain.org>
2019-06-10 00:23:33 -0700

    richtext: add blockquote and image support

---
+++ x/rich/html/blockquote.go
@@ -0,0 +1,50 @@
+// Copyright (C) 2019 rameshvk. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+package html
+
+import (
+	"strings"
+
+	"github.com/dotchain/dot/changes"
+	"github.com/dotchain/dot/changes/types"
+	"github.com/dotchain/dot/x/rich"
+)
+
+// NewBlockQuote creates a rich text with embedded block quote
+func NewBlockQuote(r rich.Text) rich.Text {
+	return rich.NewText(" ", BlockQuote{&r})
+}
+
+// BlockQuote represents a block quote with any embedded content
+type BlockQuote struct {
+	Text *rich.Text
+}
+
+// Name is the key to use with rich.Attrs
+func (bq BlockQuote) Name() string {
+	return "BlockQuote"
+}
+
+// Apply implements changes.Value.
+func (bq BlockQuote) Apply(ctx changes.Context, c changes.Change) changes.Value {
+	return (types.Generic{Set: bq.set, Get: bq.get}).Apply(ctx, c, bq)
+}
+
+func (bq BlockQuote) get(key interface{}) changes.Value {
+	return *bq.Text
+}
+
+func (bq BlockQuote) set(key interface{}, v changes.Value) changes.Value {
+	x := v.(rich.Text)
+	bq.Text = &x
+	return bq
+}
+
+// FormatHTML formats the blockQuote into HTML
+func (bq BlockQuote) FormatHTML(b *strings.Builder, f Formatter) {
+	b.WriteString("<blockquote>")
+	FormatBuilder(b, *bq.Text, f)
+	b.WriteString("</blockquote>")
+}
---
+++ x/rich/html/image.go
@@ -0,0 +1,66 @@
+// Copyright (C) 2019 rameshvk. All rights reserved.
+// Use of this source code is governed by a MIT-style license
+// that can be found in the LICENSE file.
+
+package html
+
+import (
+	"strings"
+
+	"golang.org/x/net/html"
+
+	"github.com/dotchain/dot/changes"
+	"github.com/dotchain/dot/changes/types"
+	"github.com/dotchain/dot/x/rich"
+)
+
+// NewImage creates a rich text with embedded image
+func NewImage(src, altText string) rich.Text {
+	return rich.NewText(" ", Image{src, altText})
+}
+
+// Image represents an image url
+type Image struct {
+	Src     string
+	AltText string
+}
+
+// Name is the key to use with rich.Attrs
+func (i Image) Name() string {
+	return "Image"
+}
+
+// Apply implements changes.Value.
+func (i Image) Apply(ctx changes.Context, c changes.Change) changes.Value {
+	return (types.Generic{Set: i.set, Get: i.get}).Apply(ctx, c, i)
+}
+
+func (i Image) get(key interface{}) changes.Value {
+	switch key {
+	case "Src":
+		return types.S16(i.Src)
+	case "AltText":
+		return types.S16(i.AltText)
+	}
+	return changes.Nil
+}
+
+func (i Image) set(key interface{}, v changes.Value) changes.Value {
+	switch key {
+	case "Src":
+		i.Src = string(v.(types.S16))
+	case "AltText":
+		i.AltText = string(v.(types.S16))
+	}
+	return i
+}
+
+// FormatHTML formats the image into HTML
+func (i Image) FormatHTML(b *strings.Builder, f Formatter) {
+	b.WriteString("<img src=\"")
+	b.WriteString(html.EscapeString(i.Src))
+	b.WriteString("\" alt=\"")
+	b.WriteString(html.EscapeString(i.AltText))
+	b.WriteString("\">")
+	b.WriteString("</img>")
+}
